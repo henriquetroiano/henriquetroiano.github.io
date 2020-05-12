5540563c68b16dda972749cecdbb1becf6a24bbd
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-12 15:27:16 -0300

    testando funcoes nos botoes superiores

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const selHome = document.querySelector('.menu li');
 
+function alertaHome() {
+    selHome.addEventListener('click', function() {
+        alert('window WOW')
+    })
+}
+
+alertaHome()
 
 
 
