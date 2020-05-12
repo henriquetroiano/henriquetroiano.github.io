e396e527631e7277721940690ce156097970586a
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-12 15:37:09 -0300

    correcao de bugs

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const selHome = document.querySelector('.menu li');
 
 function alertaHome() {
-    selHome.addEventListener('click', function() {
+    
         alert('window WOW')
-    })
+    
 }
 
-alertaHome()
 
 
 
