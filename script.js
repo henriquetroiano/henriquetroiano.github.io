1c72903a1af82392ef06721404f7f00161e1245e
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-18 17:15:20 -0300

    atualizacao de portifolio

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const aaa = document.querySelector('.menu li')
-const submenu = document.querySelector('.submenu')
-const clickLocation = window.addEventListener('click', function() {
-    console.log(this.event.target )
-})
 
-function alertaHome() {
-    aaa.addEventListener('click', function() {
-        submenu.classList.toggle('submenu')
-    })
-}
-
-
-
-alertaHome()
 
 
 
