932daa3d5a06c39cbfed35ce77e90ee9dd01bfea
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-19 17:05:23 -0300

    primeira tentativa de menu dropdown

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function closerr() {
-window.close()
+const getSubmenu = document.getElementsByClassName('desativado');
+const arrumado = Array.from(getSubmenu)
+
+const exibeseReverso = document.querySelector('#reverso')
+
+
+function exibir() {
+    arrumado.forEach((item, index) => { 
+       arrumado[index].classList.remove('desativado')
+    })}
+    
+function exibirReverso() {
+exibeseReverso.classList.add('desativado')
+    
 }
 
 
