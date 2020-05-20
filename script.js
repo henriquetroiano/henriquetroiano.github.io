08e9d4e0af3ad375507619c7186f9e4c74afad5b
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-20 17:13:38 -0300

    criacao do menu superior

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-const getSubmenu = document.getElementsByClassName('desativado');
-const arrumado = Array.from(getSubmenu)
-
-const exibeseReverso = document.querySelector('#reverso')
 
 
-function exibir() {
-    arrumado.forEach((item, index) => { 
-       arrumado[index].classList.remove('desativado')
-    })}
-    
-function exibirReverso() {
-exibeseReverso.classList.add('desativado')
-    
-}
 
 
 
