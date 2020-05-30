5accf15331df69ba5f2e24d43b6a7575b1878d4e
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-30 16:29:18 -0300

    criacao inicial do portifolio

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$('[href="#about_me"]').click(function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href');
+     target = $(id).offset().top;
+
+    $('html', 'body').animate({
+        scrollTop: target
+    }, 600);
+})
+
+
 
 
 
