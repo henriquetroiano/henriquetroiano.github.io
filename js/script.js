ed80a15b125e929bdcb836a642c1e21ea514db12
henriquetroiano <henriquetroiano@hotmail.com>
2020-05-31 23:15:40 -0300

    adicionando responsividade mobile

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-$('[href="#about_me"]').click(function (event) {
-    event.preventDefault();
-    var id = $(this).attr('href');
-    target = $(id).offset().top;
-
-    $('html', 'body').animate({
-        scrollTop: target
-    }, 600);
-})
 
 
 
