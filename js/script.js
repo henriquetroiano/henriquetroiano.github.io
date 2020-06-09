3fb50742d450366a4d67bdf05b725e9fbdc08825
Henrique Troiano <henrique.troiano@omel.com.br>
2020-06-09 18:34:34 -0300

    adicionando menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ function actualLocation() {
     return document.getElementsByTagName('title')[0].innerText
 }
 
+ 
+
+document.querySelector('#menu_button').addEventListener('click', function() {
+        const selMenu = document.querySelector('#menu_mobile')
+        selMenu.classList.toggle('hidden')
+})
+
 
 
 
