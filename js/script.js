61f8a71d8a75d69f3bb862d06ff1e34e09d75ae6
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-09 22:28:45 -0300

    adicionando funcao fecha menu ao clicar fora

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-
-document.querySelector('#menu_button').addEventListener('click', function () {
+// adiciona a funcao dde troca de classe ao botao do menu, que no css fará aparecer o menu
+document.querySelector('#menu_button').addEventListener('click', function (click) {
+    click.stopPropagation();
     const selMenu = document.querySelector('#menu_mobile')
     selMenu.classList.toggle('hidden')
 })
+
+// seleciona o body para definir que ele oculte o menu
+// o stopPropagation é usado no evento para que o clique no botão não seja passado para o body.
+document.querySelector('body').addEventListener('click', function (click) {
+    click.stopPropagation();
+    const selM = document.querySelector('#menu_mobile')
+    selM.classList.add('hidden')
+})
+
 
 
 
